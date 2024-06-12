afcd99b605a6a30bb4b3d918f19cbfa280970295
Jason Gledel <jasongledel@MacBook-Pro-de-Jason.local>
2024-06-12 11:21:57 +0200

    Commit version initiale

---
+++ script.js
@@ -0,0 +1,52 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+        const dropbtn = dropdown.querySelector('.dropbtn');
+
+        // Ajoute un écouteur d'événement pour le clic sur .dropbtn ou .anotherClass
+        dropbtn.addEventListener('click', function() {
+            toggleDropdown(dropdowns, dropdown);
+        });
+
+        // Si vous souhaitez ajouter une autre classe à .dropbtn, par exemple "anotherClass"
+        dropbtn.classList.add('arrowDown');
+    });
+
+
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('.dropbtn') && !event.target.closest('.dropdown')) {
+            dropdowns.forEach(dropdown => {
+                dropdown.classList.remove('show');
+            });
+        }
+    });
+
+    function toggleDropdown(dropdowns, dropdown) {
+        dropdowns.forEach(dd => {
+            if (dd !== dropdown) {
+                dd.classList.remove('show');
+            }
+        });
+        dropdown.classList.toggle('show');
+    }
+});
+
+
+
+// CLOSE DIV
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const closeBtn = document.getElementById('closeBtn');
+    const myDiv = document.getElementById('modelAspi');
+
+    closeBtn.addEventListener('click', () => {
+        myDiv.style.display = 'none';
+    });
+});
+
+
+
+
