c5a6c64315b65a8933cd7980b4d2be529cf06b6c
Jason Gledel <jasongledel@MacBook-Pro-de-Jason.local>
2024-06-19 13:47:00 +0200

    Derniers changements responsive

--- script.js
+++ script.js
@@ -54,3 +54,37 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector("body");
+const bgOpac = document.querySelector(".bg-opacite");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    bgOpac.classList.toggle("active");
+}
+
+
+
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    body.classList.remove("noscroll");
+    hamburger.classList.remove("activeHam");
+}
+
+
+
+
+
